41b1412896ff55b23e0473ef6b960271d09a9bd6
Dean McNamee <dean@gmail.com>
2013-01-21 17:57:57 +0100

    Hoist out main LZW code function. Hopefully by pulling it out from a closure / inner function, there will be only a single copy of the code shared across all instances, and the compiler can spend less time compiling the functions and optimize it better. Additionally it brings most of the GIF format related writing code together, which is a nicer read.

--- omggif.js
+++ omggif.js
@@ -102,132 +102,6 @@ function GifWriter(buf, width, height, gopts) {
     buf[p++] = 0x00;  // Terminator.
   }
 
-  // Main compression routine, palette indexes -> LZW code stream.
-  // |index_stream| must have at least one entry.
-  function outputLZWCodeStream(min_code_size, index_stream) {
-    buf[p++] = min_code_size;
-    var cur_subblock = p++;  // Pointing at the length field.
-
-    var clear_code = 1 << min_code_size;
-    var code_mask = clear_code - 1;
-    var eoi_code = clear_code + 1;
-    var next_code = eoi_code + 1;
-
-    var cur_code_size = min_code_size + 1;  // Number of bits per code.
-    var cur_shift = 0;
-    // We have at most 12-bit codes, so we should have to hold a max of 19
-    // bits here (and then we would write out).
-    var cur = 0;
-
-    function emit_bytes_to_buffer(bit_block_size) {
-      while (cur_shift >= bit_block_size) {
-        buf[p++] = cur & 0xff;
-        cur >>= 8;
-        cur_shift -= 8;
-        // If we've finished a subblock.
-        if (p - cur_subblock === 256) {
-          buf[cur_subblock] = 255;
-          cur_subblock = p++;
-        }
-      }
-    }
-
-    function emit_code(c) {
-      cur |= c << cur_shift;
-      cur_shift += cur_code_size;
-      emit_bytes_to_buffer(8);
-    }
-
-    // I am not an expert on the topic, and I don't want to write a thesis.
-    // However, it is good to outline here the basic algorithm and the few data
-    // structures and optimizations here that make this implementation fast.
-    // The basic idea behind LZW is to build a table of previously seen runs
-    // addressed by a short id (herein called output code).  All data is
-    // referenced by a code, which represents one or more values from the
-    // original input stream.  All input bytes can be referenced as the same
-    // value as an output code.  So if you didn't want any compression, you
-    // could more or less just output the original bytes as codes (there are
-    // some details to this, but it is the idea).  In order to achieve
-    // compression, values greater then the input range (codes can be up to
-    // 12-bit while input only 8-bit) represent a sequence of previously seen
-    // inputs.  The decompressor is able to build the same mapping while
-    // decoding, so there is always a shared common knowledge between the
-    // encoding and decoder, which is also important for "timing" aspects like
-    // how to handle variable bit width code encoding.
-    //
-    // One obvious but very important consequence of the table system is there
-    // is always a unique id (at most 12-bits) to map the runs.  'A' might be
-    // 4, then 'AA' might be 10, 'AAA' 11, 'AAAA' 12, etc.  This relationship
-    // can be used for an effecient lookup strategy for the code mapping.  We
-    // need to know if a run has been seen before, and be able to map that run
-    // to the output code.  Since we start with known unique ids (input bytes),
-    // and then from those build more unique ids (table entries), we can
-    // continue this chain (almost like a linked list) to always have small
-    // integer values that represent the current byte chains in the encoder.
-    // This means instead of tracking the input bytes (AAAABCD) to know our
-    // current state, we can track the table entry for AAAABC (it is guaranteed
-    // to exist by the nature of the algorithm) and the next character D.
-    // Therefor the tuple of (table_entry, byte) is guaranteed to also be
-    // unique.  This allows us to create a simple lookup key for mapping input
-    // sequences to codes (table indicies) without having to store or search
-    // any of the code sequences.  So if 'AAAA' has a table entry of 12, the
-    // tuple of ('AAAA', K) for any input byte K will be unique, and can be our
-    // key.  This leads to a integer value at most 20-bits, which can always
-    // fit in an SMI value and be used as a fast sparse array / object key.
-
-    // Output code for the current contents of the index buffer.
-    var ib_code = index_stream[0] & code_mask;  // Load first input index.
-    var code_table = { };  // Key'd on our 20-bit "tuple".
-
-    // First index already loaded, process the rest of the stream.
-    for (var i = 1, il = index_stream.length; i < il; ++i) {
-      var k = index_stream[i] & code_mask;
-      var cur_key = ib_code << 8 | k;  // (prev, k) unique tuple.
-      var cur_code = code_table[cur_key];  // buffer + k.
-
-      // Check if we have to create a new code table entry.
-      if (cur_code === undefined) {  // We don't have buffer + k.
-
-        // Emit index buffer (without k).
-        emit_code(ib_code);
-
-        if (next_code === 4096) {  // Table full, need a clear.
-          emit_code(clear_code);
-          next_code = eoi_code + 1;
-          cur_code_size = min_code_size + 1;
-          code_table = { };
-        } else {  // Table not full, insert a new entry.
-          // Increase our variable bit code sizes if necessary.  This is a bit
-          // tricky as it is based on "timing" between the encoding and
-          // decoder.  From the encoders perspective this should happen after
-          // we've already emitted the index buffer and are about to create the
-          // first table entry that would overflow our current code bit size.
-          if (next_code >= (1 << cur_code_size)) ++cur_code_size;
-          code_table[cur_key] = next_code++;  // Insert into code table.
-        }
-
-        ib_code = k;  // Index buffer to single input k.
-      } else {
-        ib_code = cur_code;  // Index buffer to sequence in code table.
-      }
-    }
-
-    emit_code(ib_code);  // There will still be something in the index buffer.
-    emit_code(eoi_code);  // End Of Information.
-
-    // Flush / finalize the sub-blocks stream to the buffer.
-    emit_bytes_to_buffer(1);
-
-    // Finish the sub-blocks, writing out any unfinished lengths and
-    // terminating with a sub-block of length 0.  If we have already started
-    // but not yet used a sub-block it can just become the terminator.
-    if (p + 1 === cur_subblock) {  // Started but unused.
-      buf[cur_subblock] = 0;
-    } else {  // Started and used, write length and additional terminator block.
-      buf[cur_subblock] = p - cur_subblock - 1;
-      buf[p++] = 0;
-    }
-  }
 
   var ended = false;
 
@@ -320,7 +194,8 @@ function GifWriter(buf, width, height, gopts) {
       }
     }
 
-    outputLZWCodeStream(min_code_size < 2 ? 2 : min_code_size, indexed_pixels);
+    p = GifWriterOutputLZWCodeStream(
+            buf, p, min_code_size < 2 ? 2 : min_code_size, indexed_pixels);
   };
 
   this.end = function() {
@@ -330,6 +205,134 @@ function GifWriter(buf, width, height, gopts) {
     }
     return p;
   };
+}
+
+// Main compression routine, palette indexes -> LZW code stream.
+// |index_stream| must have at least one entry.
+function GifWriterOutputLZWCodeStream(buf, p, min_code_size, index_stream) {
+  buf[p++] = min_code_size;
+  var cur_subblock = p++;  // Pointing at the length field.
+
+  var clear_code = 1 << min_code_size;
+  var code_mask = clear_code - 1;
+  var eoi_code = clear_code + 1;
+  var next_code = eoi_code + 1;
+
+  var cur_code_size = min_code_size + 1;  // Number of bits per code.
+  var cur_shift = 0;
+  // We have at most 12-bit codes, so we should have to hold a max of 19
+  // bits here (and then we would write out).
+  var cur = 0;
+
+  function emit_bytes_to_buffer(bit_block_size) {
+    while (cur_shift >= bit_block_size) {
+      buf[p++] = cur & 0xff;
+      cur >>= 8;
+      cur_shift -= 8;
+      // If we've finished a subblock.
+      if (p - cur_subblock === 256) {
+        buf[cur_subblock] = 255;
+        cur_subblock = p++;
+      }
+    }
+  }
+
+  function emit_code(c) {
+    cur |= c << cur_shift;
+    cur_shift += cur_code_size;
+    emit_bytes_to_buffer(8);
+  }
+
+  // I am not an expert on the topic, and I don't want to write a thesis.
+  // However, it is good to outline here the basic algorithm and the few data
+  // structures and optimizations here that make this implementation fast.
+  // The basic idea behind LZW is to build a table of previously seen runs
+  // addressed by a short id (herein called output code).  All data is
+  // referenced by a code, which represents one or more values from the
+  // original input stream.  All input bytes can be referenced as the same
+  // value as an output code.  So if you didn't want any compression, you
+  // could more or less just output the original bytes as codes (there are
+  // some details to this, but it is the idea).  In order to achieve
+  // compression, values greater then the input range (codes can be up to
+  // 12-bit while input only 8-bit) represent a sequence of previously seen
+  // inputs.  The decompressor is able to build the same mapping while
+  // decoding, so there is always a shared common knowledge between the
+  // encoding and decoder, which is also important for "timing" aspects like
+  // how to handle variable bit width code encoding.
+  //
+  // One obvious but very important consequence of the table system is there
+  // is always a unique id (at most 12-bits) to map the runs.  'A' might be
+  // 4, then 'AA' might be 10, 'AAA' 11, 'AAAA' 12, etc.  This relationship
+  // can be used for an effecient lookup strategy for the code mapping.  We
+  // need to know if a run has been seen before, and be able to map that run
+  // to the output code.  Since we start with known unique ids (input bytes),
+  // and then from those build more unique ids (table entries), we can
+  // continue this chain (almost like a linked list) to always have small
+  // integer values that represent the current byte chains in the encoder.
+  // This means instead of tracking the input bytes (AAAABCD) to know our
+  // current state, we can track the table entry for AAAABC (it is guaranteed
+  // to exist by the nature of the algorithm) and the next character D.
+  // Therefor the tuple of (table_entry, byte) is guaranteed to also be
+  // unique.  This allows us to create a simple lookup key for mapping input
+  // sequences to codes (table indicies) without having to store or search
+  // any of the code sequences.  So if 'AAAA' has a table entry of 12, the
+  // tuple of ('AAAA', K) for any input byte K will be unique, and can be our
+  // key.  This leads to a integer value at most 20-bits, which can always
+  // fit in an SMI value and be used as a fast sparse array / object key.
+
+  // Output code for the current contents of the index buffer.
+  var ib_code = index_stream[0] & code_mask;  // Load first input index.
+  var code_table = { };  // Key'd on our 20-bit "tuple".
+
+  // First index already loaded, process the rest of the stream.
+  for (var i = 1, il = index_stream.length; i < il; ++i) {
+    var k = index_stream[i] & code_mask;
+    var cur_key = ib_code << 8 | k;  // (prev, k) unique tuple.
+    var cur_code = code_table[cur_key];  // buffer + k.
+
+    // Check if we have to create a new code table entry.
+    if (cur_code === undefined) {  // We don't have buffer + k.
+
+      // Emit index buffer (without k).
+      emit_code(ib_code);
+
+      if (next_code === 4096) {  // Table full, need a clear.
+        emit_code(clear_code);
+        next_code = eoi_code + 1;
+        cur_code_size = min_code_size + 1;
+        code_table = { };
+      } else {  // Table not full, insert a new entry.
+        // Increase our variable bit code sizes if necessary.  This is a bit
+        // tricky as it is based on "timing" between the encoding and
+        // decoder.  From the encoders perspective this should happen after
+        // we've already emitted the index buffer and are about to create the
+        // first table entry that would overflow our current code bit size.
+        if (next_code >= (1 << cur_code_size)) ++cur_code_size;
+        code_table[cur_key] = next_code++;  // Insert into code table.
+      }
+
+      ib_code = k;  // Index buffer to single input k.
+    } else {
+      ib_code = cur_code;  // Index buffer to sequence in code table.
+    }
+  }
+
+  emit_code(ib_code);  // There will still be something in the index buffer.
+  emit_code(eoi_code);  // End Of Information.
+
+  // Flush / finalize the sub-blocks stream to the buffer.
+  emit_bytes_to_buffer(1);
+
+  // Finish the sub-blocks, writing out any unfinished lengths and
+  // terminating with a sub-block of length 0.  If we have already started
+  // but not yet used a sub-block it can just become the terminator.
+  if (p + 1 === cur_subblock) {  // Started but unused.
+    buf[cur_subblock] = 0;
+  } else {  // Started and used, write length and additional terminator block.
+    buf[cur_subblock] = p - cur_subblock - 1;
+    buf[p++] = 0;
+  }
+  return p;
 }
 
 try { exports.GifWriter = GifWriter; } catch(e) { }  // CommonJS.
